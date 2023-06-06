f8aa43b11142185a2e169f71ef768a2e37e329a6
tmushayahama <tmushayahama@gmail.com>
2023-06-06 16:26:40 -0700

    Added the gocam subclass from AnnotationSubgraph and process bp, mf and small mols

---
+++ base.py
@@ -0,0 +1,8 @@
+relations = {
+      'has_direct_input': 'RO:0002400',
+      'enabled_by': 'RO:0002333',
+      'has_input':  'RO:0002233',
+      'has_output': 'RO:0002234',
+      'occurs_in':  'BFO:0000066',
+      'part_of': 'BFO:0000050',
+    }
--- main.py
+++ main.py
@@ -1,32 +1,88 @@
 import pybiopax
-from collections import Counter
+import networkx as nx
+#import pydot
+from ontobio.rdfgen.gocamgen.subgraphs import AnnotationSubgraph
 import pprint
+from base import relations
 
 biopax_file = './resources/test_biopax/R-HSA-204174_level3.owl'
 model = pybiopax.model_from_owl_file(biopax_file, encoding="utf8")
-for obj in model.objects.values():
-  #pprint.pp (obj)
-  pass
- 
-stats = Counter([obj.__class__.__name__  for uid, obj in model.objects.items()]).most_common(10)
-pprint.pp(stats)
 
-for reaction in model.get_objects_by_type(pybiopax.biopax.BiochemicalReaction):
-    print('%s -> %s' % (reaction.left, reaction.right))
+class GoCAM (AnnotationSubgraph):
+    def __init__(self):
+        self.bp_node = None
+        self.mf_map = {}
+        super().__init__()
 
+    def process_pathway(self, pathway):        
+            
+        if pathway.xref:
+            self.process_bp(pathway.xref)
+
+        if pathway.pathway_order:
+            self.process_step_processes(pathway.pathway_order, pathway)
+        
+        if pathway.pathway_component:
+            self.process_components(pathway.pathway_component, pathway)
+
+    
+    def process_step_processes(self, step_processes, pathway):
+        for obj in step_processes:
+            pc = model.objects[obj.uid]
+            for sp in pc.step_process:
+                if isinstance(sp, pybiopax.biopax.Catalysis) and sp.control_type == 'ACTIVATION':
+                    self.process_mf(sp.xref, model.objects[sp.uid])
+   
+    
+    def process_components(self, components, pathway):
+        for obj in components:
+            mf = self.mf_map.get(obj.uid, None)
+
+            if mf is None:
+                continue
+
+            mf = self.add_instance_of_class(mf, is_anchor=True)
+
+            if self.bp_node is not None:
+                self.add_edge(mf, relations['part_of'], self.bp_node)
+
+            gp = self.add_instance_of_class(obj.display_name)
+            self.add_edge(mf, relations['enabled_by'], gp)
+
+            pc = model.objects[obj.uid]
+
+            if pc.left:
+                self.process_mols(pc.left, relations['has_input'], mf)
+
+            if pc.right:
+                self.process_mols(pc.right, relations['has_output'], mf)
+                
+                                        
+    def process_mf(self, xrefs, catalysis:pybiopax.biopax.Catalysis)    : 
+        for xref in xrefs:
+            if xref.db == 'GENE ONTOLOGY':
+                self.mf_map[catalysis.controlled.uid] = xref.id
+                
+                
+    def process_bp(self, xrefs): 
+        for xref in xrefs:
+            if xref.db == 'GENE ONTOLOGY':
+                bp = self.add_instance_of_class(xref.id)
+                self.bp_node = bp
+                
+
+    def process_mols(self, mols, relation, mf):
+        for mol in mols:
+            sm_node = self.add_instance_of_class(mol.display_name)
+            self.add_edge(mf, relation, sm_node)
+            
+    
+    def process_cc(self, xrefs): 
+        pass
+                        
+            
+gocam = GoCAM()
 for pathway in model.get_objects_by_type(pybiopax.biopax.Pathway):
-    name = pathway
-    print(name.uid)
-    #[pprint.pp(src.display_name) for src in pathway.data_source]
-    #print(pathway.controller_of(pybiopax.biopax.PathwayStep))
-    pprint.pp(pathway.__dict__)
-    #print(pathway.pathway_order)
-    for obj in pathway.pathway_component:
-        pprint.pp('%s -> %s' % (obj.uid, obj.display_name))
-        pc = model.objects[obj.uid]       
-            
-        for l in pc.left:
-            print('\t %s -> %s' % ('has_input',  l.display_name))
-            
-        for r in pc.right:
-            print('\t %s --> %s' % ('has_output',  r.display_name))
+    gocam.process_pathway(pathway)
+pprint.pp(nx.to_dict_of_dicts(gocam))
+print(gocam.mf_map)
