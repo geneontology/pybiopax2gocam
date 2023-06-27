aaaeda0098f44c68267c1f5c4d725bb9bd115ad0
tmushayahama <tmushayahama@gmail.com>
2023-06-27 15:02:06 -0700

    added pyvis view

--- src/views/view_factory.py
+++ src/views/view_factory.py
@@ -2,6 +2,7 @@
 from src.models.parsers.yeast_parser import YeastParser
 from src.views.gocamgen_view import GoCAMGenView
 from src.views.json_view import JSONView
+from src.views.vis_view import VisView
 
 
 class ViewFactory:
@@ -11,6 +12,8 @@ class ViewFactory:
             return JSONView(model)
         elif view_type == "gocamgen":
             return GoCAMGenView(model)
+        elif view_type == "vis":
+            return VisView(model)
        
         else:
             raise ValueError("Invalid view type")
