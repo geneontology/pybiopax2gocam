d059b814a653bf1e20a1fde5867610a0aefe4391
dustine32 <dustine32@gmail.com>
2023-07-20 10:13:48 -0700

    Almost finished with small_mol_reg rule for #5

--- src/models/transformation_strategy.py
+++ src/models/transformation_strategy.py
@@ -1,4 +1,10 @@
 from src.models.processing_strategy import ProcessingStrategy
+from src.models.biopax_model import Relationship
+
+
+IS_SMALL_MOLECULE_REGULATOR_OF = "RO:0012004"
+IS_SMALL_MOLECULE_ACTIVATOR_OF = "RO:0012005"
+IS_SMALL_MOLECULE_INHIBITOR_OF = "RO:0012006"
 
 
 class TransformationStrategyFactory(ProcessingStrategy):
@@ -29,8 +35,20 @@ class ReactomeTransformationStrategy(TransformationStrategy):
 
     def _infer_small_molecule_regulators(self, data):
         changed_data = data
-        # Iterate pathways then reactions
-        # If not Catalysis and ControlType == "INHIBITION", "ACTIVATION"
-        #
-        # Look for RO_0002429, RO_0002430 small molecules
+        for pathway in data.pathways:
+            for rxn in pathway.reactions:
+                if isinstance(rxn.controller, list):  # We should remove this conditional after changing dataclass
+                    for c in rxn.controller:
+                        c_entity_type = c.control_entity.type
+                        # Really this should be 'if c_entity_type is descendant of chemical entity CHEBI:24431  but not
+                        # descendant of nucleic acid CHEBI:33696'. For now, just see if 'CHEBI:'
+                        if c_entity_type.startswith("CHEBI:"):
+                            # Also figure out if + or - from ControlType
+                            rel_type = IS_SMALL_MOLECULE_REGULATOR_OF
+                            if c.control_type == "ACTIVATION":
+                                rel_type = IS_SMALL_MOLECULE_ACTIVATOR_OF
+                            elif c.control_type == "INHIBITION":
+                                rel_type = IS_SMALL_MOLECULE_INHIBITOR_OF
+                            relationship = Relationship(c.control_entity.instance_id, rxn.molecular_function.instance_id, rel_type)
+                            pathway.relationships.append(relationship)
         return changed_data
