7dc8ded660681c43b19feb8fdfb5540245817283
dustine32 <dustine32@gmail.com>
2023-07-27 13:24:51 -0700

    Changed expected prefix in test case

--- tests/test_reactome.py
+++ tests/test_reactome.py
@@ -16,4 +16,4 @@ def test_chebi_extraction():
                     found_chebis.append(c.id)
 
     # Hint: it's in the xrefs of entity_reference of controller NADH
-    assert 'CHEBI:57945' in found_chebis
+    assert 'ChEBI:57945' in found_chebis
